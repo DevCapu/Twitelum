6bebf68e5de51c09efdc5bdfa30bac125e1abde8
Felipe Moreno Borges <cyberlegend48@gmail.com>
2019-11-19 02:13:23 -0300

    Adicionando listener de clique no botão de login

--- app/src/main/java/br/com/devcapu/twitelum/ui/activity/LoginActivity.java
+++ app/src/main/java/br/com/devcapu/twitelum/ui/activity/LoginActivity.java
@@ -2,15 +2,47 @@ package br.com.devcapu.twitelum.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import br.com.devcapu.twitelum.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        configuraBotaoLogin();
+    }
+
+    private void configuraBotaoLogin() {
+        Button botaoEntrar = findViewById(R.id.activity_login_button_entrar);
+        botaoEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText campoUsuario = findViewById(R.id.activity_login_input_usuario);
+                EditText campoSenha = findViewById(R.id.activity_login_input_senha);
+
+                String valorCampoUsuario = campoUsuario.getText().toString();
+                Log.i("Campo usuário", "valorCampoUsuario: " + valorCampoUsuario);
+                String valorCampoSenha = campoSenha.getText().toString();
+                Log.i("Campo usuário", "valorCampoSenha: " + valorCampoSenha);
+
+                if (valorCampoUsuario.equals("DevCapu") && valorCampoSenha.equals("123456")) {
+                    Toast.makeText(context, "Entrou", Toast.LENGTH_LONG)
+                            .show();
+                } else {
+                    Toast.makeText(context, "Não entrou", Toast.LENGTH_LONG)
+                            .show();
+                }
+            }
+        });
     }
 }
