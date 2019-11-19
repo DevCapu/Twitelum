beaf1e0f6f5a2610aa89d2735f899a14da80a845
Felipe Moreno Borges <cyberlegend48@gmail.com>
2019-11-19 01:54:29 -0300

    Inicializando Projeto

---
+++ app/src/main/java/br/com/devcapu/twitelum/ui/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package br.com.devcapu.twitelum.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.devcapu.twitelum.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
